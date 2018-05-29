fc0e42157932a1fcbea107e2d4ac385ad5b68c07
sanyi <sanyi@169.254.16.179>
2018-05-29 17:20:34 +0200

    Homework

---
+++ variables.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+let a = 2;
+let b = (a === 3);
+let c = "My choice of words";
+let d = [a,b,c];
+let e = {a: a,b: b,c: c};
+
+
