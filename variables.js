fe32caa53539f6731fb860817696f9216d76b813
sanyi <sanyi@169.254.16.179>
2018-06-05 15:12:46 +0200

    Homework

--- variables.js
+++ variables.js
@@ -11,4 +11,14 @@ let c = "My choice of words";
 let d = [a,b,c];
 let e = {a: a,b: b,c: c};
 
+//reverses d, iterates through it and displays it
+d.reverse();
+for(let i=0;i<d.length;i++) {
+    console.log(d[i]);
+};
+ 
+ //iterates through e and displays it
+for(var index in e) {
+  console.log( index + " : " + e[index]);
+};
 
